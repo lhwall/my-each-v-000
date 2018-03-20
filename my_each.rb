c440580fe85dc3a5979ff5f91959d629a20a6230
Lucy Wall <lhwall@gmail.com>
2018-03-20 17:27:33 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
+require 'pry'
+
+def my_each(array) # put argument(s) here
   # code here
+  i = 0 
+  while array.length < i 
+   i += 1
+   yield array[i]
+   end
+ #binding.pry
+end
+
+collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+  puts i
 end
