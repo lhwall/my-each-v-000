ebfb26156de1106fb0bedc84669ec7419cc8194e
Lucy Wall <lhwall@gmail.com>
2018-03-20 17:36:14 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
+require 'pry'
+
+def my_each(array) # put argument(s) here
   # code here
+  i = 0 
+  while array.length > i 
+   yield array[i]
+   i += 1
+   end
+return array
 end
