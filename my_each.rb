7f8d12c70067c02fc55e39386119e6f773a0b0d5
Lucy Wall <lhwall@gmail.com>
2018-03-20 17:29:33 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -7,10 +7,5 @@ def my_each(array) # put argument(s) here
    i += 1
    yield array[i]
    end
- binding.pry
-end
-
-collection = [1, 2, 3, 4]
-my_each(collection) do |i|
-  puts i
+ #binding.pry
 end
